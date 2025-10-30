33a9b3408aa16a1d93d4b444e778d115e1040b3b
Sergio Eder Cervantes Rincon <al232682@edu.uaa.mx>
2025-10-30 17:40:55 -0600

    Reproductor exoplayer jalando

--- app/src/main/java/com/example/examenreproductor/MainActivity.kt
+++ app/src/main/java/com/example/examenreproductor/MainActivity.kt
@@ -1,15 +1,35 @@
 package com.example.examenreproductor
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
 
 class MainActivity : AppCompatActivity() {
+
+    private var player: ExoPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
 
+        player = ExoPlayer.Builder(this).build()
+
+        val playButton: Button = findViewById(R.id.playButton)
+
+        playButton.setOnClickListener {
+            val mediaItem = MediaItem.fromUri(Uri.parse("android.resource://$packageName/${R.raw.development}"))
+            player?.setMediaItem(mediaItem)
+            player?.prepare()
+            player?.play()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        player?.release()
+        player = null
     }
 }
-J
