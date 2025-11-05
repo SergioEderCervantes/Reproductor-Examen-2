57b5f6d3d3347da6fba91f5344b1630f1029b3fb
Sergio Eder Cervantes Rincon <al232682@edu.uaa.mx>
2025-11-04 22:46:09 -0600

    Cambio al min api 32

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.examenreproductor"
-        minSdk = 34
+        minSdk = 32
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
